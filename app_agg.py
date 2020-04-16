143c0836e4aa9d8bcfdeaed6a1dfb522a36038bf
nesturie <58046407+nesturie@users.noreply.github.com>
2020-04-16 12:59:20 +0100

    Data aggregation
    The app_agg.py allows the user to select the file. It is suggested to select this sample -> journey_locations_f1.csv

---
+++ app_agg.py
@@ -0,0 +1,112 @@
+import os
+import streamlit as st 
+
+# EDA Pkgs
+import pandas as pd 
+
+# Viz Pkgs
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns 
+
+
+def main():
+	
+	st.title("Bagboard")
+	st.subheader("Datasets  Explorer for Bagboard")
+
+
+	def file_selector(folder_path='./'):
+		filenames = os.listdir(folder_path)
+		selected_filename = st.selectbox("Select A file ",filenames)
+		return os.path.join(folder_path,selected_filename)
+
+	filename = file_selector()
+	st.info("You Selected {}".format(filename))
+
+	# Read Data
+	df = pd.read_csv(filename)
+	# Show Dataset
+
+
+
+	# Show Columns
+	if st.button("Column Names"):
+		st.write(df.columns)
+
+	# Show Shape
+	if st.checkbox("Shape of Dataset"):
+		data_dim = st.radio("Show Dimension By ",("Rows","Columns"))
+		if data_dim == 'Rows':
+			st.text("Number of Rows")
+			st.write(df.shape[0])
+		elif data_dim == 'Columns':
+			st.text("Number of Columns")
+			st.write(df.shape[1])
+		else:
+			st.write(df.shape)
+
+	# Select Columns
+	if st.checkbox("Select Columns To Show"):
+		all_columns = df.columns.tolist()
+		selected_columns = st.multiselect("Select",all_columns)
+		new_df = df[selected_columns]
+		st.dataframe(new_df)
+	
+	# Show Values
+	if st.button("Value Counts"):
+		st.text("Value Counts By Target/Class")
+		st.write(df.iloc[:,-1].value_counts())
+
+
+	# Show Datatypes
+	if st.button("Data Types"):
+		st.write(df.dtypes)
+
+
+
+	# Show Summary
+	if st.checkbox("Summary"):
+		st.write(df.describe().T)
+
+
+	
+
+
+	# Customizable Plot
+
+	st.subheader("Customizable Plot")
+	all_columns_names = df.columns.tolist()
+	type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
+	selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+
+	if st.button("Generate Plot"):
+		st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
+
+		# Plot By Streamlit
+		if type_of_plot == 'area':
+			cust_data = df[selected_columns_names]
+			st.area_chart(cust_data)
+
+		elif type_of_plot == 'bar':
+			cust_data = df[selected_columns_names]
+			st.bar_chart(cust_data)
+
+		elif type_of_plot == 'line':
+			cust_data = df[selected_columns_names]
+			st.line_chart(cust_data)
+
+		# Custom Plot 
+		elif type_of_plot:
+			cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
+			st.write(cust_plot)
+			st.pyplot()
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
